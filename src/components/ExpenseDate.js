792600bf965bf3d32f228676b7d78352178cf902
mridulgelal <mridul.gelal@gmail.com>
2022-05-19 15:37:14 +0545

    first commit upto splitting component into multiple components

---
+++ src/components/ExpenseDate.js
@@ -0,0 +1,19 @@
+import './ExpenseDate.css';
+function ExpenseDate(props){
+
+const month= props.date.toLocaleString('en-Us',{month:'long'});
+const day= props.date.toLocaleString('en-Us',{day:'2-digit'});
+const year= props.date.getFullYear();
+
+
+return (
+    <div className='expense-date'>
+        <div className='expese-date__month'>{month}</div>
+    <div className='expese-date__day'>{day}</div>
+    <div className='expese-date__year'></div>{year}</div>
+
+
+);
+}
+
+export default ExpenseDate;
